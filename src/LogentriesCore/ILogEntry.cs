92c391c876a73594a6d75a3f1699196d82daa041
Nick Moores <nick@moores-home.co.uk>
2015-10-24 15:12:01 +0100

    Added LogEntry class

---
+++ src/LogentriesCore/ILogEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogentriesCore
+{
+    internal interface ILogEntry
+    {
+        string Line { get; }
+    }
+}
---
+++ src/LogentriesCore/LogEntry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogentriesCore
+{
+    internal sealed class LogEntry : ILogEntry
+    {
+        public LogEntry(string line)
+        {
+            Debug.Assert(line != null, $"{nameof(line) != null}");
+
+            this.Line = line;
+        }
+
+        public string Line { get; }
+
+        public override string ToString()
+        {
+            return this.Line;
+        }
+    }
+}
